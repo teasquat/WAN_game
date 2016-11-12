fe2ddc86d3e4915cab1d59c2dbd42ca884cda539
Lord Niels N. Horn <i.am@nilq.dk>
2016-11-12 19:28:35 +0100

    Add test lit object

--- love/main.lua
+++ love/main.lua
@@ -9,14 +9,16 @@ love.load = function()
       55,
       55
     },
-    refractionStrength = 32,
+    refractionStrength = 12,
     reflectionVisibility = 0.75
   })
-  light_mouse = light_world:newLight(0, 0, 255, 127, 63, 300)
-  light_mouse:setGlowStrength(0.3)
+  light_mouse = light_world:newLight(0, 0, 255, 127, 73, 400)
+  light_mouse:setGlowStrength(0.001)
   rect = light_world:newRectangle(512, 512, 64, 64)
+  circ = light_world:newCircle(light_mouse.x, light_mouse.y + 50, 37)
 end
 love.update = function(dt)
+  love.window.setTitle("_business(" .. tostring(love.timer.getFPS()) .. ")")
   light_mouse:setPosition((love.mouse.getX() - camera.x) / camera.scale, (love.mouse.getY() - camera.y) / camera.scale)
   light_world:update(dt)
   return light_world:setTranslation(camera.x, camera.y, camera.scale)
@@ -26,8 +28,10 @@ love.draw = function()
     return light_world:draw(function()
       love.graphics.setColor(255, 255, 255)
       love.graphics.rectangle("fill", -camera.x / camera.scale, -camera.y / camera.scale, love.graphics.getWidth() / camera.scale, love.graphics.getHeight() / camera.scale)
-      love.graphics.setColor(63, 255, 127)
-      return love.graphics.polygon("fill", rect:getPoints())
+      love.graphics.setColor(50, 255, 50)
+      local cx, cy = circ:getPosition()
+      love.graphics.circle("fill", cx, cy, circ:getRadius())
+      return print(rect.x, rect.y, rect:getPoints())
     end)
   end)
 end
--- love/main.moon
+++ love/main.moon
@@ -7,16 +7,20 @@ love.load = ->
   export camera       = gamera.new 0, 0, love.graphics.getWidth!, love.graphics.getHeight!
   export light_world  = light {
     ambient:              {55, 55, 55}
-    refractionStrength:   32
+    refractionStrength:   12
     reflectionVisibility: 0.75
   }
 
-  export light_mouse = light_world\newLight 0, 0, 255, 127, 63, 300
-  light_mouse\setGlowStrength 0.3
+  export light_mouse = light_world\newLight 0, 0, 255, 127, 73, 400
+  light_mouse\setGlowStrength 0.001
 
   export rect = light_world\newRectangle 512, 512, 64, 64
+  export circ = light_world\newCircle light_mouse.x, light_mouse.y + 50, 37
 
 love.update = (dt) ->
+
+  love.window.setTitle "_business(#{ love.timer.getFPS! })"
+
   light_mouse\setPosition (love.mouse.getX! - camera.x) / camera.scale, (love.mouse.getY! - camera.y) / camera.scale
 
   light_world\update dt
@@ -28,5 +32,6 @@ love.draw = ->
       love.graphics.setColor 255, 255, 255
       love.graphics.rectangle "fill", -camera.x / camera.scale, -camera.y / camera.scale, love.graphics.getWidth! / camera.scale, love.graphics.getHeight! / camera.scale
 
-      love.graphics.setColor 63, 255, 127
-      love.graphics.polygon "fill", rect\getPoints!
+      love.graphics.setColor 50, 255, 50
+      cx, cy = circ\getPosition!
+      love.graphics.circle "fill", cx, cy, circ\getRadius!
